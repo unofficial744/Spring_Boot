f163141762e3f851c83eaab1cde6eea3adf90468
Aditya <adityaroyak6@gmail.com>
2026-09-02 21:37:15 +0530

    Main.java file

---
+++ C1/12_Aug_26/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    void add(){
+        int a = 10;
+        int b = 30;
+        int c = a+b;
+        System.out.println(c);
+    }
+
+    public static void main(String[] args) {
+      Main obj = new Main();
+      obj.add();  
+    }
+}
